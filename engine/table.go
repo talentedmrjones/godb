2b4a4efd3101fd2851b347e849b781234ef52553
Richard Jones <the@talentedmrjones.com>
2014-11-15 20:13:04 -0700

    referencing the now unexported struct fields

--- engine/table.go
+++ engine/table.go
@@ -10,10 +10,10 @@ import (
 
 /* NON-EXPORTED METHODS */
 
-// setTableFileSize is used internally to update the file size of the table.
+// settableFileSize is used internally to update the file size of the table.
 // The table's file size is used to demarcate EOF which is used for indexing.
-func setTableFileSize (tbl *Table, newSize int) {
-  tbl.TableFileSize += int64(newSize)
+func settableFileSize (tbl *Table, newSize int) {
+  tbl.tableFileSize += int64(newSize)
 }
 
 /* EXPORTED METHODS */
@@ -33,6 +33,11 @@ func NewTable (chunkSize uint32, path string, tableFileSize int64) (*Table) {
   return &table
 }
 
+// ChunkSize returns the Tables chunk size
+func (tbl *Table) ChunkSize () (uint32) {
+  return tbl.chunkSize
+}
+
 
 // Create writes a record to the table's .godbd file.
 // It returns an error|nil or on success a map[string]string (the data that it was original given)
@@ -44,7 +49,7 @@ func (tbl *Table) Create (data map[string]string) (error, map[string]string) {
     return err, nil
   }
 
-  if _, primaryIndexExists := tbl.PrimaryIndex[data["id"]]; primaryIndexExists {
+  if _, primaryIndexExists := tbl.primaryIndex[data["id"]]; primaryIndexExists {
     // id exists in index, therefore not new
     return errors.New("ID_NON_UNIQUE"), nil
   }
@@ -62,21 +67,23 @@ func (tbl *Table) Create (data map[string]string) (error, map[string]string) {
   // ensure that the data is not larger than table chunk size
   // TODO: test
 
-  if uint32(bufferLength)>tbl.ChunkSize {
+  if uint32(bufferLength)>tbl.chunkSize {
     bufferLengthErr :=  errors.New("TOO_LARGE")
     return bufferLengthErr, nil
   }
 
   //  pad the buffer out to the chunkSize
-  b.Write(bytes.Repeat([]byte{0}, int(tbl.ChunkSize)-bufferLength))
+  b.Write(bytes.Repeat([]byte{0}, int(tbl.chunkSize)-bufferLength))
+
+  // determine position by looking to freeChunks
 
   // initialize primary index as array
   // record position of record in index
-  tbl.PrimaryIndex[data["id"]] = tbl.TableFileSize
+  tbl.primaryIndex[data["id"]] = tbl.tableFileSize
 
-  tbl.TableFile.WriteAt(b.Bytes(), tbl.TableFileSize)
+  tbl.tableFile.WriteAt(b.Bytes(), tbl.tableFileSize)
 
-  setTableFileSize(tbl, b.Len())
+  settableFileSize(tbl, b.Len())
 
   return nil, data
 }
@@ -94,19 +101,19 @@ func (tbl *Table) Read (data map[string]string) (error, map[string]string) {
 
   // find the offset and byte length from index
   id := data["id"]
-  if _, indexOk := tbl.PrimaryIndex[id]; !indexOk {
+  if _, indexOk := tbl.primaryIndex[id]; !indexOk {
     // wasn't found
     err := errors.New("NOT_FOUND")
     return err,nil
   }
 
-  primaryIndex := tbl.PrimaryIndex[id]
+  primaryIndex := tbl.primaryIndex[id]
 
   // make a []byte using the length stored at index[1]
-  buf := make([]byte, tbl.ChunkSize)
+  buf := make([]byte, tbl.chunkSize)
 
   // read starting at the offset stored at index[0]
-  _, readErr := tbl.TableFile.ReadAt(buf, primaryIndex)
+  _, readErr := tbl.tableFile.ReadAt(buf, primaryIndex)
   if readErr != nil {
     panic(readErr)
   }
@@ -136,7 +143,7 @@ func (tbl *Table) Update (data map[string]string) (error, map[string]string) {
   }
 
   // ensure record index contains the record
-  if _, indexOk := tbl.PrimaryIndex[data["id"]]; !indexOk {
+  if _, indexOk := tbl.primaryIndex[data["id"]]; !indexOk {
     // wasn't found
     err := errors.New("NOT_FOUND")
     return err, nil
@@ -155,17 +162,17 @@ func (tbl *Table) Update (data map[string]string) (error, map[string]string) {
   // ensure that the data is not larger than table chunk size
   // TODO: test
 
-  if uint32(bufferLength)>tbl.ChunkSize {
+  if uint32(bufferLength)>tbl.chunkSize {
     bufferLengthErr :=  errors.New("TOO_LARGE")
     return bufferLengthErr, nil
   }
 
   //  pad the buffer out to the chunkSize
-  b.Write(bytes.Repeat([]byte{0}, int(tbl.ChunkSize)-bufferLength))
+  b.Write(bytes.Repeat([]byte{0}, int(tbl.chunkSize)-bufferLength))
 
-  tbl.TableFile.WriteAt(b.Bytes(), tbl.PrimaryIndex[data["id"]])
+  tbl.tableFile.WriteAt(b.Bytes(), tbl.primaryIndex[data["id"]])
 
-  setTableFileSize(tbl, b.Len())
+  settableFileSize(tbl, b.Len())
 
   return nil, data
 }
@@ -181,16 +188,16 @@ func (tbl *Table) Delete (data map[string]string) (error) {
   }
 
   // ensure record index contains the record
-  if _, indexOk := tbl.PrimaryIndex[data["id"]]; !indexOk {
+  if _, indexOk := tbl.primaryIndex[data["id"]]; !indexOk {
     // wasn't found
     return nil
   }
 
   // mark chunk available for reuse
-  tbl.FreeChunks[tbl.PrimaryIndex[data["id"]]]=1
+  tbl.freeChunks[tbl.primaryIndex[data["id"]]]=1
 
   // delete record from the index
-  delete(tbl.PrimaryIndex,data["id"])
+  delete(tbl.primaryIndex,data["id"])
 
   return nil
 
